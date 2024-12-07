8d41d6cd7e666b998185adf6f0ee842ccffc3241
Marko Vujovic <markovujovic1604@gmail.com>
2024-12-07 14:46:52 +0100

    Create UsersPageComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {UsersPageComponent} from './pages/users-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'users',
+    pathMatch: 'full',
+  },
+  {
+    path: 'users',
+    component: UsersPageComponent
+  }
+];
---
+++ src/app/pages/users-page.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users-page.component.html',
+  styleUrls: ['./users-page.component.scss'],
+})
+export class UsersPageComponent {
+
+}
+
